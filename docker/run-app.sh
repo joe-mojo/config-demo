9edee1dd2bdffe962c4739bcc413b372da72fcf3
Joachim Rousseau <joachim.rousseau@wescale.fr>
2020-12-30 18:23:21 +0100

    Fixed launcher, reversed loading order, upgraded version

--- docker/run-app.sh
+++ docker/run-app.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
+
+if [ "$1" = "" ]
+then
+  APP_VERSION=$(grep version ../build.sbt | grep -v value | cut -d '=' -f 2 | cut -d '"' -f 2)
+else
+  APP_VERSION=$1
+fi
+
 mkdir -p /tmp/config-demo-volumes/config-volume && \
 cp ../src/main/resources/application-runtime.conf /tmp/config-demo-volumes/config-volume/ && \
-docker run -v /tmp/config-demo-volumes/config-volume:/config-demo/config joe-mojo/config-demo:0.1.0
+docker run -v /tmp/config-demo-volumes/config-volume:/config-demo/config joe-mojo/config-demo:$APP_VERSION
